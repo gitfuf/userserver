534a07f7af1b1a58893f2dd362b021a0cc680f1e
Maria Solomina <mariaakg@gmail.com>
2018-07-20 21:23:14 +0300

    add panic and request log middleware

--- usecases/route.go
+++ usecases/route.go
@@ -3,6 +3,7 @@ package usecases
 import (
 	"log"
 	"net/http"
+	"time"
 
 	"github.com/gorilla/mux"
 	"github.com/julienschmidt/httprouter"
@@ -22,7 +23,7 @@ func (app *ServerApp) initRESTApi() {
 	httpRouter.POST("/user", app.newUser)
 	//gorillaRouter.HandleFunc("/user", app.newUser).Methods("POST")
 
-	//use standart mux for simple requests (actually better to use httprouter, ut for test purpose will stay so )
+	//use standart mux for simple requests (actually better to use httprouter, but for test purpose will stay so )
 	stdRouter := http.NewServeMux()
 	stdRouter.HandleFunc("/shutdown", app.serverShutdown)
 
@@ -30,5 +31,32 @@ func (app *ServerApp) initRESTApi() {
 	serverMux.Handle("/user/", gorillaRouter)
 	serverMux.Handle("/user", httpRouter)
 	serverMux.Handle("/shutdown", stdRouter)
-	app.Server.Handler = serverMux
+
+	//set middleware
+	serverHandler := requestsLogMiddleware(serverMux)
+	serverHandler = panicMiddleware(serverHandler)
+
+	app.Server.Handler = serverHandler
+}
+
+func panicMiddleware(mw http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		//log.Println("panicMiddleware ", r.URL.Path)
+		defer func() {
+			if err := recover(); err != nil {
+				log.Printf("recovered from error: %v ", err)
+				http.Error(w, http.StatusText(500), 500)
+			}
+		}()
+		mw.ServeHTTP(w, r)
+	})
+}
+
+func requestsLogMiddleware(mw http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		log.Println("requestsLogMiddleware ", r.URL.Path)
+		start := time.Now()
+		mw.ServeHTTP(w, r)
+		log.Printf("[%s] %s, %s %s\n", r.Method, r.RemoteAddr, r.URL.Path, time.Since(start))
+	})
 }
